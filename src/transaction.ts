0a2129af2c22b4373650d3840746249f2386d7f6
Aaron Faanes <dafrito@gmail.com>
2021-01-14 15:26:49 -0600

    Implement library

--- src/transaction.ts
+++ src/transaction.ts
@@ -5,7 +5,7 @@ export class Server {
   constructor(url:string) {
     this._url = url;
   }
-  
+
   url():string {
     return this._url;
   }
@@ -18,23 +18,126 @@ export class Server {
           'Content-Type': 'application/json',
       },
     });
-    return resp;
+    return resp.data.id;
   }
-}
+
+  async deleteAccount(account:Account) {
+    if (typeof account.id() !== "number") {
+      throw new Error("Account ID must be given");
+    }
+    const url = this.url() + "/accounts/" + account.id();
+    return await axios.delete(url);
+  }
+
+  async getAccount(id:number) {
+    const resp = await axios.get(this.url() + "/accounts/" + id);
+    const {name} = resp.data;
+    return new Account(id, name);
+  }
+
+  async createCategory(category:Category) {
+    const url = this.url() + "/category/";
+    const resp = await axios.post(url, category.json(), {
+      headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+      },
+    });
+    return resp.data.id;
+  }
+
+  async deleteCategory(category:Category) {
+    if (typeof category.id() !== "number") {
+      throw new Error("Category ID must be given");
+    }
+    const url = this.url() + "/category/" + category.id();
+    return await axios.delete(url);
+  }
+
+  async getCategory(id:number) {
+    const resp = await axios.get(this.url() + "/category/" + id);
+    const {name} = resp.data;
+    return new Category(id, name);
+  }}
 
 export class Account {
+  _id:number;
   _name:string;
-  constructor(name:string) {
+  constructor(id:number, name:string) {
+    this._id = id;
     this._name = name;
+  }
+
+  id() {
+    return this._id;
+  }
+
+  name() {
+    return this._name;
   }
 
   json() {
     return {
+      id:this._id,
       name:this._name
     }
   }
 
-  save(server:Server) {
-    return server.createAccount(this);
+  async save(server:Server) {
+    const id = await server.createAccount(this);
+    this._id = id;
+    return id;
+  }
+
+  async delete(server:Server) {
+    return server.deleteAccount(this);
+  }
+}
+
+export class Category {
+  _id:number;
+  _name:string;
+  _parent:Category;
+
+  constructor(id:number, name:string, parent?:Category) {
+    this._id = id;
+    this._name = name;
+    this._parent = parent;
+  }
+
+  id() {
+    return this._id;
+  }
+
+  name() {
+    return this._name;
+  }
+
+  parent() {
+    return this._parent;
+  }
+
+  json() {
+    const data = {
+      id:this._id,
+      name:this._name,
+      parent:null as number
+    };
+
+    if (this.parent()) {
+      data.parent = this.parent().id();
+    }
+
+    return data;
+  }
+
+  async save(server:Server) {
+    const id = await server.createCategory(this);
+    this._id = id;
+    return id;
+  }
+
+  async delete(server:Server) {
+    return server.deleteCategory(this);
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,12 +1,44 @@
 var assert = require("assert");
-import {Server, Account} from "../dist/transaction";
+import {Server, Account, Category} from "../dist/transaction";
 import '@babel/polyfill';
 
 describe("Account", function () {
-  it("works", async ()=>{
+  it("can be created and deleted", async ()=>{
     const server = new Server("http://127.0.0.1:8000");
-    const acct = new Account("A fancy name");
-    const resp = await acct.save(server);
+    const acct = new Account(null, "A fancy name");
+    let resp = await acct.save(server);
+    assert.ok(resp);
+    resp = await acct.delete(server);
+    assert.ok(resp);
+  });
+  it("can be retrieved", async ()=>{
+    const server = new Server("http://127.0.0.1:8000");
+    const created = new Account(null, "A fancy name");
+    await created.save(server);
+    const retrieved = await server.getAccount(created.id())
+    assert.strictEqual(created.name(), retrieved.name());
+    const resp = await created.delete(server);
     assert.ok(resp);
   });
 });
+
+describe("Category", function () {
+  it("can be created and deleted", async ()=>{
+    const server = new Server("http://127.0.0.1:8000");
+    const cat = new Category(null, "A fancy name", null);
+    let resp = await cat.save(server);
+    assert.ok(resp);
+    resp = await cat.delete(server);
+    assert.ok(resp);
+  });
+  it("can be retrieved", async ()=>{
+    const server = new Server("http://127.0.0.1:8000");
+    const created = new Category(null, "A fancy name", null);
+    await created.save(server);
+    const retrieved = await server.getCategory(created.id())
+    assert.strictEqual(created.name(), retrieved.name());
+    const resp = await created.delete(server);
+    assert.ok(resp);
+  });
+});
+
