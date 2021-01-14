620474d8eb3ce57aa498d1104ebb90ca0e7bef58
Aaron Faanes <dafrito@gmail.com>
2021-01-14 13:43:01 -0600

    Rename to transaction

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-DIST_NAME = TODO-PACKAGE-NAME
+DIST_NAME = transaction
 
 SCRIPT_FILES = \
 	src/$(DIST_NAME).ts
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/TODO-PACKAGE-NAME.ts"),
+  entry: path.resolve(__dirname, "src/transaction.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "TODO-PACKAGE-NAME.js",
+    filename: "transaction.js",
     globalObject: "this",
-    library: "TODO-PACKAGE-NAME",
+    library: "transaction",
     libraryTarget: "umd",
   },
   module: {
